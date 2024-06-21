ac6b2d92204e3a8f475ae0169a5956e1b1dfc50d
Anand Jain <solidsnake745@gmail.com>
2024-06-21 15:03:45 -0400

    Implemented invert output setting to ByteNoteRegister
    Added setting and functionality around being able to invert individual outputs of ByteNoteRegister. Also added tests to verify. Relates to #48.

--- src/Common/ByteNoteRegister.h
+++ src/Common/ByteNoteRegister.h
@@ -10,6 +10,7 @@
 		inline static SerialDebug _debug = SerialDebug(DEBUG_BYTENOTEREGISTER);
 		
 		uint8_t _data = 0;
+		uint8_t _invert = 0;
 		NoteDuration _durations[8];
 		NoteDuration _maxDurations[8];
 			
@@ -30,10 +31,31 @@
 			};
 			#pragma GCC pop_options
 		
-			inline uint8_t getByteValue() { return _data; };
-			inline bool getBit(uint8_t i) { return bitRead(_data, i); };
+			inline uint8_t getByteValue() 
+			{ 
+				if(_invert > 0)
+					return ((~_data & _invert) + (_data & ~_invert));
+				
+				return _data;
+			};
+			
 			inline void setBit(uint8_t i) { bitSet(_data, i); };
-		  
+			inline bool getBit(uint8_t i) { return bitRead(_data, i); };			
+			inline bool getBitValue(uint8_t i)
+			{ 
+				bool value = bitRead(_data, i);
+				return isInverted(i) ? !value : value;
+			};
+			
+			inline bool isInverted(uint8_t i) { return bitRead(_invert, i); };
+			inline void setInverted(uint8_t i, bool value)
+			{
+				if(value)
+					bitSet(_invert, i);
+				else
+					bitClear(_invert, i);
+			};
+			
 			inline void clearBit(uint8_t i)
 			{
 				bitClear(_data, i);
@@ -78,19 +100,110 @@
 				}
 			};
 			
-			#if INCLUDE_TESTS
+		#if INCLUDE_TESTS
+		private:
+			inline static void printBits(uint8_t value)
+			{
+				for(int x = 7; x >= 0; x--) 
+					_debug.debug(5, "%d", bitRead(value, x));
+			}
+		
+		public: 
 			inline static void runTest()
 			{
-				ByteNoteRegister test;
-				_debug.debugln(5, F("Initial value of _data: %d"), test.getByteValue());
+				//getBit/setBit tests
+				ByteNoteRegister test;			
+				//_data should default to 0 at this point
+				_debug.debugln(5, F("Initial value of _data before setting: %d"), test.getByteValue());
 				for(int x = 0; x < 8; x++)
 				{
 					test._data = 0;
 					test.setBit(x);
-					_debug.debugln(5, F("Value after bit %d set: %d"), x, test.getByteValue());
+					_debug.debug(5, F("Bit %d set: %d\t"), x, test.getByteValue());
+					printBits(test.getByteValue());
+					_debug.debugln(5);
 				}
+				
+				test._data = 255;
+				_debug.debugln(5, F("Initial value of _data before clearing: %d"), test.getByteValue());
+				for(int x = 0; x < 8; x++)
+				{
+					test._data = 255;
+					test.clearBit(x);
+					_debug.debug(5, F("Bit %d cleared: %d (255 - %d)\t"), x, test.getByteValue(), 1 << x);
+					printBits(test.getByteValue());
+					_debug.debugln(5);
+				}
+				
+				//Invert tests
+				_debug.debugln(5, F("Invert logic confirmation tests"));
+				
+				test._data = B10101010;
+				uint8_t invertMask = B01010101;
+				
+				_debug.debug(5, F("Initial value:\t\t")); printBits(test._data); _debug.debugln(5);
+				_debug.debug(5, F("Invert Mask:\t\t")); printBits(invertMask); _debug.debugln(5);
+				
+				//Inverted portion (Invert and then mask with invert mask) + non-inverted portion (Mask with inverted invert mask)				
+				uint8_t invertedPortion = (~test._data & invertMask);
+				uint8_t nonInvertedPortion = (test._data & ~invertMask);
+				_debug.debug(5, F("Inverted portion:\t")); printBits(invertedPortion); _debug.debugln(5);
+				_debug.debug(5, F("Non-inverted portion:\t")); printBits(nonInvertedPortion); _debug.debugln(5);
+				
+				//Combination should result in all 1s
+				_debug.debug(5, F("Combined:\t\t")); printBits(invertedPortion + nonInvertedPortion); _debug.debugln(5);
+				
+				test._invert = B11110000;
+				_debug.debug(5, F("Initial value of _invert before setting: %d ("), test._invert);
+				printBits(test._invert);
+				_debug.debugln(5, F(")"));
+				
+				for(int x = 0; x < 8; x++)
+				{
+					//setInverted basically uses the same logic as setBit/clearBit
+					//So no need to test every index - testing each path on half the values
+					test._invert = B11110000;
+					if(x >= 4)
+					{
+						test.setInverted(x, false);
+						_debug.debug(5, F("Bit %d cleared: %d\t"), x, test._invert);
+					}
+					else
+					{
+						test.setInverted(x, true);
+						_debug.debug(5, F("Bit %d set: %d\t\t"), x, test._invert);
+					}
+					
+					printBits(test._invert);
+					_debug.debugln(5);
+				}
+				
+				_debug.debugln(5, F("Invert top 4 bits using _invert setting"));
+				test._data = B00001111;
+				test._invert = B11110000;
+				_debug.debug(5, F("Initial value:\t")); printBits(test._data); _debug.debugln(5);
+				_debug.debug(5, F("Invert Mask:\t")); printBits(test._invert); _debug.debugln(5);
+				_debug.debug(5, F("Result:\t\t")); printBits(test.getByteValue()); _debug.debugln(5);
+				
+				_debug.debugln(5, F("Invert bottom 4 bits using _invert setting"));
+				test._data = B00001111;
+				test._invert = B00001111;
+				_debug.debug(5, F("Initial value:\t")); printBits(test._data); _debug.debugln(5);
+				_debug.debug(5, F("Invert Mask:\t")); printBits(test._invert); _debug.debugln(5);
+				_debug.debug(5, F("Result:\t\t")); printBits(test.getByteValue()); _debug.debugln(5);
+				
+				_debug.debugln(5, F("Invert middle 4 bits using _invert setting"));
+				test._data = B00001111;
+				test._invert = B00111100;
+				_debug.debug(5, F("Initial value:\t")); printBits(test._data); _debug.debugln(5);
+				_debug.debug(5, F("Invert Mask:\t")); printBits(test._invert); _debug.debugln(5);
+				_debug.debug(5, F("Result:\t\t")); printBits(test.getByteValue()); _debug.debugln(5);
+				
+				_debug.debugln(5, F("Use getBitValue to print the same result from the last test"));
+				_debug.debug(5, F("getBitValue:\t"));
+				for(int x = 7; x >= 0; x--) _debug.debug(5, "%d", test.getBitValue(x));
+				_debug.debugln(5);
 			}
-			#endif
+		#endif
 	};
-  
 #endif
